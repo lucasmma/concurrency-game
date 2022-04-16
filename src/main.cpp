08c7d00a5138967d2a194c198b281447f90d073f
Lucas Mendonça <lucasmmamaral@gmail.com>
2022-04-16 14:58:46 -0300

    feat: add consistancy to input playerNumbers

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,18 @@
 
 int main(int argc, char const **argv) {
   bool exit = false;
-
   if(argc < 2){
     std::cout << "Digite um número de jogador ao executar o programa" << std::endl;
     return 0;
+  } else if(argc > 2){
+    std::cout << "Digite apenas número de jogador ao executar o programa" << std::endl;
+    return 0;
+  } else {
+    int playerNumber = (int)(*argv[1] - '0');
+    if(playerNumber != 1 || playerNumber != 2) {
+      std::cout << "Os números de jogadores diponíveis são apenas o 1 e o 2" << std::endl;
+      return 0;
+    }
   }
   while(!exit){
     int number;
@@ -21,6 +29,7 @@ int main(int argc, char const **argv) {
     else if(number == 1){
       Game game = Game::getInstance();
       game.run((int)(*argv[1] - '0'), true);
+      std::cout <<  std::endl;
     }
     else if (number == 2){
       exit = true;
