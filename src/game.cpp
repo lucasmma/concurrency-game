20d1c7ce7722e69ecf0ca64434ceb9184eb9a4e9
Lucas Mendonça <lucasmmamaral@gmail.com>
2022-04-17 16:18:19 -0300

    feat: run condition fixed

--- src/game.cpp
+++ src/game.cpp
@@ -22,57 +22,61 @@ Game::~Game(){
 }
 
 void Game::run(bool autoPlay, int playerNumber){
-  std::cout << "O jogo de inuncação começou" << std::endl;
   // std::cout << "AutoPlay -> " << autoPlay<< std::endl;
   if(!autoPlay) {
+    std::cout << "O jogo de inuncação começou" << std::endl;
     sem_wait(&(state->sem));
     state->render();
     sem_post(&(state->sem));
   }
   sem_wait(&(state->sem));
+  // std::cout << "AutoPlay depois do wait-> " << autoPlay << std::endl;
   while (!state->isGameFinished()) {
+    // std::cout << "--------------------------------------------------> " << autoPlay << std::endl;
     sem_post(&(state->sem));
-    sem_wait(&(state->cinSem));
-    std::cout << std::endl << "Esperou o cin => " << playerNumber << std::endl << std::endl;
-    if(!autoPlay) {
+    // std::cout << std::endl << "Esperou o cin => " << playerNumber << std::endl << std::endl;
+    sem_wait(&(state->sem));
+    if(state->playsCounter % 2 + 1 == playerNumber){
+      sem_post(&(state->sem));
+      
+      sem_wait(&(state->cinSem));
       sem_wait(&(state->sem));
-      state->render();
-      sem_post(&(state->sem));
-    }
-    sem_wait(&(state->sem));
-    if(state->isGameFinished()){
-      sem_post(&(state->sem));
-      return;
-    }
-    sem_post(&(state->sem));
-    std::vector<int> inputHandled;
-    sem_wait(&(state->sem));
-    if(!autoPlay){
-      std::cout << std::endl << "Vez do jogador "<< playerNumber << std::endl;
-      inputHandled = handleInput();
-      while(!state->isSpotAvailableOnBoard(inputHandled, playerNumber)){
-        inputHandled = handleInput(true);
-      }
-    } else if(state->playsCounter % 2 + 1 == playerNumber){
-      // std::this_thread::sleep_for(std::chrono::seconds(2));
-      // std::cout << std::endl << "esperado" << std::endl;
-      inputHandled = state->autoPlay(playerNumber);
-    }
-    sem_post(&(state->sem));
-    sem_post(&(state->cinSem));
-    
-    sem_wait(&(state->sem));
-    state->update(inputHandled, playerNumber);
-    sem_post(&(state->sem));
-
-    sem_wait(&(state->sem));
-    if(state->isGameFinished()){
       if(!autoPlay) {
         state->render();
       }
+      if(state->isGameFinished()){
+        sem_post(&(state->sem));
+        return;
+      }
       sem_post(&(state->sem));
+      // std::cout << "jogando " << playerNumber << " --------------------------------> " << autoPlay << std::endl;
+      sem_wait(&(state->sem));
+      std::vector<int> inputHandled;
+      if(!autoPlay){
+        // std::cout << std::endl << "Vez do jogador "<< playerNumber << std::endl;
+        inputHandled = handleInput();
+        while(!state->isSpotAvailableOnBoard(inputHandled, playerNumber)){
+          inputHandled = handleInput(true);
+        }
+      } else {
+        std::cout << std::endl << "--------------------------------" << std::endl;
+        std::cout << std::endl << "-------autoplay jogando---------" << std::endl;
+        std::cout << std::endl << "--------------------------------" << std::endl;
+        inputHandled = state->autoPlay(playerNumber);
+        // std::this_thread::sleep_for(std::chrono::seconds(2));
+      }
+      sem_post(&(state->cinSem));
+      sem_post(&(state->sem));
+
+      sem_wait(&(state->sem));
+      state->update(inputHandled, playerNumber);
+      if(state->isGameFinished()){
+        if(!autoPlay) {
+          state->render();
+        }
+        sem_post(&(state->sem));
+      }
     }
-    
   }
 }
 
